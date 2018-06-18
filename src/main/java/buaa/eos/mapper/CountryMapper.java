c1db2dadff9f3dfe8bafcf2451fd2de4c0c9177a
buaapyj <541726449@qq.com>
2018-06-18 18:14:28 +0800

    first commit

---
+++ src/main/java/buaa/eos/mapper/CountryMapper.java
@@ -0,0 +1,7 @@
+package buaa.eos.mapper;
+
+import buaa.eos.model.Country;
+import buaa.eos.util.MyMapper;
+
+public interface CountryMapper extends MyMapper<Country> {
+}
---
+++ src/test/java/buaa/eos/test/CountryServiceTest.java
@@ -0,0 +1,34 @@
+package buaa.eos.test;
+
+import buaa.eos.service.CountryService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+import buaa.eos.Application;
+import buaa.eos.model.Country;
+import java.util.List;
+
+/**
+ * @author liuzh
+ * @since 2017/9/2.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Import(Application.class)
+public class CountryServiceTest {
+
+    @Autowired
+    private CountryService countryService;
+
+    @Test
+    public void test() {
+        Country country = new Country();
+        List<Country> all = countryService.getAll(country);
+        for (Country c : all) {
+            System.out.println(c.getCountryname());
+        }
+    }
+}
